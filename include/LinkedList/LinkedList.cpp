4552fefe2ccc4e5ae07d12ccb40b0a141a647c14
Даниил Иванов <v1mes2004@gmail.com>
2023-11-14 19:25:18 +0400

    completed class, no tests

--- include/LinkedList/LinkedList.cpp
+++ include/LinkedList/LinkedList.cpp
@@ -6,6 +6,7 @@
 #include <stdexcept> 
 #include <random>
 
+using namespace std;
 
 namespace Linkedlist {
 
@@ -35,7 +36,7 @@ namespace Linkedlist {
 		void push_head(const LinkedList<T>& other);
 		void pop_head();
 		void pop_tail();
-		void delete_node(const T& value);
+		void delete_node(const T& val);
 		T& operator[](int index);
 		const T operator[](int index) const;
 		Node<T>* get_head() const {
@@ -81,6 +82,196 @@ namespace Linkedlist {
 			delete tmp;
 			tmp = nullptr;
 		}
+	}
+
+	template<typename T>
+	LinkedList<T>& LinkedList<T>::operator=(const LinkedList& other) {
+		if (this == &other) {
+			return *this;
+		}
+		while (head) {
+			pop_head();
+		}
+		Node<T>* tmp = other.head;
+		while (tmp) {
+			push_tail(tmp->value);
+			tmp = tmp->next;
+		}
+		return *this;
+	}
+
+	template<typename T>
+	void LinkedList<T>::push_tail(const T& value) {
+		Node<T>* newNode = new Node<T>(value);
+		if (head == nullptr) {
+			head = newNode;
+		}
+		else {
+			Node<T>* current = head;
+			while (current->next != nullptr) {
+				current = current->next;
+			}
+			newNode->prev = current;
+			current->next = newNode;
+
+		}
+	}
+
+	template<typename T>
+	void LinkedList<T>::push_head(const T& value) {
+		Node<T>* newNode = new Node<T>(value);
+		if (head == nullptr) {
+			head = newNode;
+		}
+		else {
+			Node<T>* current = head;
+			head = newNode;
+			head->next = current;
+			current->prev = head;
+		}
+	}
+
+	template<typename T>
+	void LinkedList<T>::pop_head() {
+		if (head == nullptr) {
+			throw length_error("Cannot delete element in empty list");
+		}
+		else {
+			if (head->next == nullptr) {
+				delete head;
+				head = nullptr;
+			}
+			else {
+				Node<T>* tmp = head;
+				head = head->next;
+				head->prev = nullptr;
+				delete tmp;
+				tmp = nullptr;
+			}
+		}
+	}
+
+	template<typename T>
+	void LinkedList<T>::pop_tail() {
+		if (head == nullptr) {
+			throw length_error("Cannot delete element in empty list");
+		}
+		else {
+			Node<T>* last = head;
+			while (last->next != nullptr) {
+				last = last->next;
+			}
+			Node<T>* pre_last = last->prev;
+			if (pre_last != nullptr) {
+				pre_last->next = nullptr;
+			}
+			else {
+				head = nullptr;
+			}
+			delete last;
+			last = nullptr;
+
+
+		}
+	}
+
+	template<typename T>
+	void LinkedList<T>::push_tail(const LinkedList<T>& other) {
+		if (other.head != nullptr) {
+			Node<T>* tmp_other = other.head;
+			while (tmp_other) {
+				this->push_tail(tmp_other->value);
+				tmp_other = tmp_other->next;
+			}
+		}
+	}
+
+	template<typename T>
+	void LinkedList<T>::push_head(const LinkedList<T>& other) {
+		if (other.head != nullptr) {
+			Node<T>* tmp_other = other.head;
+			while (tmp_other) {
+				this->push_head(tmp_other->value);
+				tmp_other = tmp_other->next;
+			}
+		}
+	}
+
+	template<typename T>
+	T& LinkedList<T>::operator[](const int index) {
+		Node<T>* tmp = head;
+		int count = 0;
+		while (tmp != nullptr) {
+			if (count == index) {
+				return tmp->value;
+			}
+			else {
+				tmp = tmp->next;
+				count ++;
+			}
+		}
+		throw out_of_range("Index out of range");
+	}
+
+	template<typename T>
+	const T LinkedList<T>::operator[](const int index) const{
+		Node<T>* tmp = head;
+		int count = 0;
+		while (tmp != nullptr) {
+			if (count == index) {
+				return tmp->value;
+			}
+			else {
+				tmp = tmp->next;
+				count++;
+			}
+		}
+		throw out_of_range("Index out of range");
+	}
+
+	template<typename T>
+	void LinkedList<T>::delete_node(const T& val) {
+		if (head != nullptr) {
+			Node<T>* current = head;
+			Node<T>* tmp = nullptr;
+			while (current != nullptr) {
+				tmp = current->next;
+				if (current->value == val) {
+					if (current->prev == nullptr) {
+						this->pop_head();
+					}
+					else {
+						if (current->next == nullptr) {
+							this->pop_tail();
+						}
+						else {
+							current->next->prev = current->prev;
+							current->prev->next = current->next;
+							delete current;
+						}
+					}
+				}
+				current = tmp;
+			}
+		}
+		else {
+			throw length_error("Empty list");
+		}
+	}
+
+	template<typename T>
+	ostream& operator<<(std::ostream& stream, LinkedList<T>& rhs)
+	{
+		Node<T>* tmp = rhs.get_head();
+		if (tmp == nullptr) {
+			stream << "Empty list";
+			return stream;
+		}
+		while (tmp != nullptr) {
+			stream << tmp->value << " ";
+			tmp = tmp->next;
+		}
+		return stream;
 	}
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -5,4 +5,19 @@ using namespace Linkedlist;
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "RUS");
+
+	LinkedList<int> list1;
+	list1.push_tail(2);
+	list1.push_tail(3);
+	list1.push_tail(5);
+	cout << list1 <<endl;
+
+	LinkedList<int> list2;
+	list2.push_tail(2);
+	list2.push_tail(3);
+
+	cout << list2 << endl;
+	list1.push_tail(list2);
+	cout << list1 << endl;
 }
