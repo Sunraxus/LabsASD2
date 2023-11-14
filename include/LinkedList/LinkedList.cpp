c6f80cf0380496e6670422a259c7ea5cd0d47c7c
Даниил Иванов <v1mes2004@gmail.com>
2023-11-14 17:42:18 +0400

    implementation of constructors and destructor

--- include/LinkedList/LinkedList.cpp
+++ include/LinkedList/LinkedList.cpp
@@ -36,9 +36,51 @@ namespace Linkedlist {
 		void pop_head();
 		void pop_tail();
 		void delete_node(const T& value);
-		T& operator[](const int index);
+		T& operator[](int index);
 		const T operator[](int index) const;
-		Node<T>* get_head() const;
+		Node<T>* get_head() const {
+			return head;
+		};
 	};
+
+	template<typename T>
+	LinkedList<T>::LinkedList() : head(nullptr) {}
+
+	template<typename T>
+	LinkedList<T>::LinkedList(const LinkedList<T>& other) : head(nullptr) {
+		Node<T>* tmp_other = other.head;
+		while (tmp_other) {
+			this->push_tail(tmp_other->value);
+			tmp_other = tmp_other->next;
+		}
+	}
+
+	template<typename T>
+	LinkedList<T>::LinkedList(const int size, const int random_capacity) : head(nullptr) {
+		int count = 0;
+		while (count != size) {
+			this->push_tail((int)(rand()) * random_capacity / RAND_MAX + 1);
+			count += 1;
+		}
+	}
+
+	template<typename T>
+	LinkedList<T>::LinkedList(const int size, const float random_capacity) : head(nullptr) {
+		int count = 0;
+		while (count != size) {
+			this->push_tail((float)(rand()) * (random_capacity - 0.0001f) / RAND_MAX + 0.0001f);
+			count += 1;
+		}
+	}
+
+	template<typename T>
+	LinkedList<T>::~LinkedList() {
+		while (head) {
+			Node<T>* tmp = head;
+			head = head->next;
+			delete tmp;
+			tmp = nullptr;
+		}
+	}
 }
 #endif
