97bc277e928f0e84b37312a49808c7fea12557e3
Даниил Иванов <v1mes2004@gmail.com>
2023-11-14 17:04:52 +0400

    creating a dummy class

--- include/LinkedList/LinkedList.cpp
+++ include/LinkedList/LinkedList.cpp
@@ -9,6 +9,36 @@
 
 namespace Linkedlist {
 
-}
+	template<typename T>
+	class Node {
+	public:
+		T value;
+		Node<T>* next;
+		Node<T>* prev;
+		Node<T>(const T& value) :value(value), next(nullptr), prev(nullptr) {};
+	};
 
+	template<typename T>
+	class LinkedList {
+	private:
+		Node<T>* head;
+	public:
+		LinkedList();
+		LinkedList(const LinkedList<T>& other);
+		LinkedList(const int size, const int random_capacity);
+		LinkedList(const int size, const float random_capacity);
+		~LinkedList();
+		LinkedList& operator=(const LinkedList<T>& other);
+		void push_tail(const T& value);
+		void push_tail(const LinkedList<T>& other);
+		void push_head(const T& value);
+		void push_head(const LinkedList<T>& other);
+		void pop_head();
+		void pop_tail();
+		void delete_node(const T& value);
+		T& operator[](const int index);
+		const T operator[](int index) const;
+		Node<T>* get_head() const;
+	};
+}
 #endif
